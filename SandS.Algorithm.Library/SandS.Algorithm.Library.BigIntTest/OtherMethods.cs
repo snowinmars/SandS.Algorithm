e5677cdab995db4cd2f6e4213ea348ff2070e521
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-08-03 15:09:24 +0300

    Added test project for BigInt. Moved solutions to another folder.

---
+++ SandS.Algorithm.Library/SandS.Algorithm.Library.BigIntTest/OtherMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandS.Algorithm.Library.BigIntTest
+{
+    public class OtherMethods
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(123)]
+        [InlineData(9875)]
+        [InlineData(123456789123)]
+        [InlineData(4045432065982464)]
+        public void MethodDeepCloneMustWork(uint v)
+        {
+            bigint num = new bigint(v);
+
+            Assert.Equal<bigint>(num, num.DeepClone());
+        }
+    }
+}
