077f0bd008713b0939dfe8b77b6093cc0f495cc7
snowonmars <stuff4work@yandex.ru>
2016-09-10 09:27:16 +0300

    Adfded CoupleColelction class

---
+++ SandS.Algorithm.Library.OtherTest/PlainDictionaryTests.cs
@@ -0,0 +1,136 @@
+﻿using SandS.Algorithm.CommonNamespace;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace SandS.Algorithm.Library.Other
+{
+    public class PlainDictionaryTest
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(16)]
+        [InlineData(1024)]
+        public void CCCtorMustWork_Positive(int capacity)
+        {
+            var cc = new CoupleCollection<int, int>(capacity);
+
+            Assert.True(cc.Count == 0);
+            Assert.NotNull(cc.Keys);
+            Assert.NotNull(cc.Values);
+
+            var en = cc.GetEnumerator();
+
+            Assert.Throws(typeof(InvalidOperationException), () => { var a = en.Current; });
+            Assert.False(en.MoveNext());
+
+            ////
+
+            var cc3 = new CoupleCollection<string, string>(capacity);
+
+            Assert.True(cc3.Count == 0);
+            Assert.NotNull(cc3.Keys);
+            Assert.NotNull(cc3.Values);
+
+            var en2 = cc3.GetEnumerator();
+
+            Assert.Throws(typeof(InvalidOperationException), () => { var a = en2.Current; });
+            Assert.False(en2.MoveNext());
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        public void CCCtorMustWork_Negative(int capacity)
+        {
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => { var cc = new CoupleCollection<int, int>(capacity); });
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => { var cc = new CoupleCollection<string, string>(capacity); });
+
+        }
+
+        [Theory]
+        [InlineData(0,0)]
+        [InlineData(0, 1)]
+        [InlineData(1, 0)]
+        [InlineData(19, 17)]
+        [InlineData(17, 17)]
+        public void CCAddMustAddOneElmnent(int x, int y)
+        {
+            var cc = new CoupleCollection<int, int>();
+
+            cc.Add(x, y);
+
+            Assert.True(cc.Count == 1, "! cc.Count == 1");
+            Assert.True(cc.Keys.Count == 1, "! cc.Keys.Count == 1");
+            Assert.True(cc.Values.Count == 1, "! cc.Values.Count == 1");
+            Assert.True(cc.Contains(new KeyValuePair<int, int>(x, y)), "! cc.Contains(new KeyValuePair<int, int>(x, y))");
+            Assert.True(cc.ContainsKey(x), "! cc.ContainsKey(x)");
+            Assert.True(cc.ContainsValue(y), "! cc.ContainsValue(y)");
+            Assert.True(cc.FindKeyPosition(x) == 0, "! cc.FindKeyPosition(x) == 0");
+            Assert.True(cc.FindValuePosition(y) == 0, "! cc.FindValuePosition(y) == 0");
+
+            int value;
+
+            Assert.True(cc.TryGetValue(x, out value), "! cc.TryGetValue(x, out value)");
+            Assert.True(value == y, "! value == y");
+            Assert.False(cc.TryGetValue(11, out value), "! cc.TryGetValue(11, out value)"); // 11 must not came as inline data
+            Assert.True(value == 0, "! value == 0");
+
+            if (x != y)
+            {
+                Assert.False(cc.ContainsValue(x), "! cc.ContainsValue(x)");
+                Assert.False(cc.ContainsKey(y), "! cc.ContainsKey(y)");
+                Assert.True(cc.FindKeyPosition(y) == -1, "! cc.FindKeyPosition(y) == -1");
+                Assert.True(cc.FindValuePosition(x) == -1, "! cc.FindValuePosition(x) == -1");
+            }
+        }
+
+        [Theory]
+        [InlineData(19, 17)]
+        public void CCAddMustRemoveOneElmnent(int x, int y)
+        {
+            var cc = new CoupleCollection<int, int>();
+
+            cc.Add(x, y);
+            cc.Add(x*2, y*3);
+            cc.Add(x*5, y*7);
+            cc.Remove(x*2);
+
+            Assert.True(cc.Count == 2, "! cc.Count == 2");
+            Assert.True(cc.Keys.Count == 2, "! cc.Keys.Count == 2");
+            Assert.True(cc.Values.Count == 2, "! cc.Values.Count == 2");
+
+            Assert.True(cc.Contains(new KeyValuePair<int, int>(x, y)), "! cc.Contains(new KeyValuePair<int, int>(x, y))");
+            Assert.True(cc.Contains(new KeyValuePair<int, int>(x*5, y*7)), "! cc.Contains(new KeyValuePair<int, int>(x*5, y*7))");
+            Assert.False(cc.Contains(new KeyValuePair<int, int>(x*2, y*3)), "! cc.Contains(new KeyValuePair<int, int>(x*2, y*3))");
+
+            Assert.True(cc.ContainsKey(x), "! cc.ContainsKey(x)");
+            Assert.True(cc.ContainsValue(y), "! cc.ContainsValue(y)");
+            Assert.True(cc.ContainsKey(x*5), "! cc.ContainsKey(x*5)");
+            Assert.True(cc.ContainsValue(y*7), "! cc.ContainsValue(y*7)");
+            Assert.False(cc.ContainsKey(x*2), "! cc.ContainsKey(x*2)");
+            Assert.False(cc.ContainsValue(y*3), "! cc.ContainsValue(y*3)");
+
+            Assert.True(cc.FindKeyPosition(x) == 0, "! cc.FindKeyPosition(x) == 0");
+            Assert.True(cc.FindValuePosition(y) == 0, "! cc.FindValuePosition(y) == 0");
+            Assert.True(cc.FindKeyPosition(x*5) == 1, "! cc.FindKeyPosition(x*5) == 1");
+            Assert.True(cc.FindValuePosition(y*7) == 1, "! cc.FindValuePosition(y*7) == 1");
+            Assert.True(cc.FindKeyPosition(x*2) == -1, "! cc.FindKeyPosition(x*2) == -1");
+            Assert.True(cc.FindValuePosition(y*3) == -1, "! cc.FindValuePosition(y*3) == -1");
+
+            int value;
+
+            Assert.True(cc.TryGetValue(x, out value), "! cc.TryGetValue(x, out value)");
+            Assert.True(value == y, "! value == y");
+            Assert.True(cc.TryGetValue(x*5, out value), "! cc.TryGetValue(x*5, out value)");
+            Assert.True(value == y*7, "! value == y*7");
+            Assert.False(cc.TryGetValue(11, out value), "! cc.TryGetValue(11, out value)"); // 11 must not came as inline data
+            Assert.True(value == 0, "! value == 0");
+        }
+    }
+}
---
+++ SandS.Algorithm.Library/SandS.Algorithm.Library.Other/CoupleCollectionEnumerator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandS.Algorithm.Library.Other
+{
+    public class CoupleCollectionEnumerator<TKey, TValue> : IEnumerator<KeyValuePair<TKey, TValue>>
+    {
+        private IList<TKey> Keys { get; set; }
+        private IList<TValue> Values { get; set; }
+
+        public CoupleCollectionEnumerator(IEnumerable<TKey> keys, IEnumerable<TValue> values)
+        {
+            this.Keys = keys.ToList();
+            this.Values = values.ToList();
+
+            if (this.Keys.Count != this.Values.Count)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private int position = -1;
+
+        public KeyValuePair<TKey, TValue> Current
+        {
+            get
+            {
+                if (position < 0 || position > this.Keys.Count)
+                {
+                    throw new InvalidOperationException($"At position {position} Current is undefined");
+                }
+
+                return new KeyValuePair<TKey, TValue>(this.Keys[this.position],
+                                                        this.Values[this.position]);
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return this.Current;
+            }
+        }
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            if (this.position >= 0 &&
+                    this.position < this.Keys.Count)
+            {
+                ++this.position;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            this.position = -1;
+        }
+    }
+}
